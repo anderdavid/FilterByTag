9faafc3db5a46bd9e651c081eb89e676454c7780
anderdavid <anderdavid86@gmail.com>
2022-02-20 13:58:33 -0500

    set token 2

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-console.log('hello world  set token');
-
-/* git remote set-url origin  https://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git@github.com:anderdavid/FilterByTag.git */
+console.log('hello world set token 2');
